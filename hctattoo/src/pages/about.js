9f18bcc9bb587ce62880002914db1560cc12602d
Kriptzy <106583411+Kriptzy@users.noreply.github.com>
2023-06-06 09:08:09 +0100

    Add files via upload

---
+++ hctattoo/src/pages/about.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+    <div className="about-container">
+      <div className="about-image-container">
+        <div className="background-about"></div>
+        <div className="about-title-container">
+          <div className="about-title">Sobre</div>
+        </div>
+      </div>
+    </div>
+    <div className="about-text">
+        colocar o sobre mim.
+      </div>
+    </div>
+  );
+};
---
+++ hctattoo/src/pages/home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+    <div className="home-container">
+      <div className="home-image-container">
+        <div className="background-home"></div>
+        <div className="home-title-container">
+          <div className="home-title">Inicio</div>
+        </div>
+      </div>
+    </div>
+    <div className="home-text-container">
+      <div className="home-text">Bem-vindo</div>
+      <div className="home-text">ao</div>
+      <div className="home-text">Hardcore Tattoo & Body Piercing</div>
+    </div>
+    </div>
+  );
+}
